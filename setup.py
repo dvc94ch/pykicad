8b56d671b8de90f5cd1b923a98a380d1fb023fdb
Mark Harfouche <mark.harfouche@gmail.com>
2019-12-07 08:51:07 -0500

    Add versioneer

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup, find_packages
+import versioneer
 
 setup(
     name='pykicad',
     packages=find_packages(),
-    version='0.1.1',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     description='Library for working with KiCAD file formats',
     long_description=open('README.md').read(),
     author='David Craven',
